addb95ccfc2c81bbe91e2e0569c3775b9f62fcd3
Pedr <pedr.browne@gmail.com>
2024-07-24 14:06:43 +0100

    chore: Remove unused eslint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,11 +21,7 @@ module.exports = {
     },
   },
 
-  extends: [
-    'eslint:recommended',
-    'plugin:import/recommended',
-    'plugin:react/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:import/recommended'],
 
   plugins: ['import'],
 
