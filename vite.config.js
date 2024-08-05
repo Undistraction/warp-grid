e314ed96d9f771c06cf1abe955c2112f8e9954b2
Pedr <pedr.browne@gmail.com>
2024-08-05 19:46:42 +0100

    fix: Switch ts module type for single-file type export
    BREAKING CHANGE

--- vite.config.js
+++ vite.config.js
@@ -35,7 +35,7 @@ export default defineConfig(() => {
     plugins: [
       // Generate a single types file for all our types
       dts({
-        // rollupTypes: true,
+        rollupTypes: true,
       }),
     ],
   }
