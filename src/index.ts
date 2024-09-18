12f09345270208902c7373633b382d8b69571771
Pedr <pedr.browne@gmail.com>
2024-09-18 15:45:25 +0100

    fix: Re-export types from coons-patch

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,15 @@
 
 export * from './types'
 
+// Re-export types we are using from coons-patch
+export type {
+  BoundingCurves,
+  Curve,
+  InterpolatePointOnCurve,
+  Point,
+  Points,
+} from 'coons-patch'
+
 // -----------------------------------------------------------------------------
 // Re-export Interpolation functions
 // -----------------------------------------------------------------------------
