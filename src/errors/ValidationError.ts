0a1d9efc4ed77d81187fcee05543c761f1c16517
Pedr <pedr.browne@gmail.com>
2026-02-22 09:47:09 +0000

    fix: Set ValidationError name

--- src/errors/ValidationError.ts
+++ src/errors/ValidationError.ts
@@ -1,6 +1,7 @@
 export class ValidationError extends Error {
   constructor(message = `A validation error occurred`) {
     super(message)
+    this.name = `ValidationError`
     this.message = message
   }
 }
